e52d91abed3e7a732cfc4f0adf4d9be8d4584c47
Aliaksandr Chernahrebel <Aliaksandr_Chernahrebel@epam.com>
2018-02-20 18:26:40 +0300

    Added API test code

---
+++ BlogAPI/Controllers/PostsController.cs
@@ -0,0 +1,36 @@
+﻿using BlogAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BlogAPI.Controllers
+{
+    public class PostsController : ApiController
+    {
+        Post[] posts = new Post[]
+        {
+            new Post { Id = 1, Title = "Title1", Text = "Text1", UserName = "User1" },
+            new Post { Id = 2, Title = "Title2", Text = "Text2", UserName = "User2" },
+            new Post { Id = 3, Title = "Title3", Text = "Text3", UserName = "User3" },
+        };
+
+        public IEnumerable<Post> GetAllPosts()
+        {
+            return posts;
+        }
+
+        public IHttpActionResult GetPosts(int id)
+        {
+            var post = posts.FirstOrDefault((p) => p.Id == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(post);
+        }
+    }
+}
---
+++ BlogAPI/Models/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlogAPI.Models
+{
+    public class Comment
+    {
+        public int PostId { get; set; }
+
+        public string Text { get; set; }
+
+        public string UserName { get; set; }
+    }
+}
